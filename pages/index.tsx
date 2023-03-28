8801fea586da3cd7f41ac3d129b009e5d03cdb9c
mihaiicey <mihai@icey.ro>
2023-03-28 17:11:55 +0300

    fix mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,8 @@ const Home: NextPage = () => {
   return (
     <>
 <div className="h-screen flex flex-col justify-center items-center">
-  <h1 className="text-2xl  text-center font-extrabold w-1/3 mb-3 text-blue-900">Discuții cu clienții și refuzurile: un ghid REVOLUTIONAR (pe bune) pentru a-ți păstra mintea sănătoasă și spiritul înalt!</h1>
-  <p className="text-base text-center w-1/3 mb-6">Salut, știu prin ce emoții treci când discuți cu un client și ajungi la punctul decisiv. Bazat pe multe studii, am ajuns să descopăr metoda REVOLUȚIONARĂ prin care te poți recompensa sau să-ți RIDICI MORALUL LA CER în cazul în care te-a refuzat.</p>
+  <h1 className="text-2xl  text-center font-extrabold md:w-1/3 w-full px-2 md:px-0 mb-3 text-blue-900">Discuții cu clienții și refuzurile: un ghid REVOLUTIONAR (pe bune) pentru a-ți păstra mintea sănătoasă și spiritul înalt!</h1>
+  <p className="text-base text-center  md:w-1/3 w-full px-2 mb-6">Salut, știu prin ce emoții treci când discuți cu un client și ajungi la punctul decisiv. Bazat pe multe studii, am ajuns să descopăr metoda REVOLUȚIONARĂ prin care te poți recompensa sau să-ți RIDICI MORALUL LA CER în cazul în care te-a refuzat.</p>
   <div className="flex flex-col sm:flex-row justify-between items-center">
     <Link href="/milion" className="bg-green-500 text-white py-2 px-4 mb-4 sm:mr-2 sm:mb-0">Am prins un contract</Link>
     <Link href="/altu-sa-fie" className="bg-red-500 text-white py-2 px-4">Nu am prins contract</Link>
